3d300bbf372352bf182ceefd695c2f68b10d1613
venturions <alex.eduardo10@hotmail.com>
2023-06-09 21:37:20 -0300

    estrutura do projeto

--- App.tsx
+++ App.tsx
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import {
+  useFonts,
+  Inter_500Medium,
+  Inter_700Bold,
+} from '@expo-google-fonts/inter'
+import { colors, fonts } from './src/themes/theme'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_500Medium,
+    Inter_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Estrutura</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    height: '100%',
+    backgroundColor: colors.purple,
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  text: {
+    color: 'white',
+    fontFamily: fonts.regular,
+    textAlign: 'center',
+  },
+})
---
+++ src/themes/theme.ts
@@ -0,0 +1,21 @@
+export const colors = {
+  blueDark: '#1E6F9F',
+  blue: '#4EA8DE',
+  purpleDark: '#5E60CE',
+  purple: '#8284FA',
+
+  gray700: '#0D0D0D',
+  gray600: '#1A1A1A',
+  gray500: '#262626',
+  gray400: '#333333',
+  gray300: '#808080',
+  gray200: '#D9D9D9',
+  gray100: '#F2F2F2',
+
+  danger: '#E25858',
+}
+
+export const fonts = {
+  regular: 'Inter_500Medium',
+  bold: 'Inter_700Bold',
+}
