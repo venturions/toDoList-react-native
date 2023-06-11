6a7b33eaf14e1f70705c740af4388b3682af67e3
venturions <alex.eduardo10@hotmail.com>
2023-06-11 01:02:06 -0300

    validação tarefa existente

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -11,6 +11,7 @@ import { AntDesign } from '@expo/vector-icons'
 import { defaulTheme } from '../../themes/theme'
 import { TaskList } from '../../components/TaskList'
 import useTaskStore from '../../stores/taskStore'
+import { Alert } from 'react-native'
 
 export interface TaskProps {
   description: string
@@ -21,8 +22,16 @@ export default function Home() {
   const [taskName, setTaskName] = useState('')
 
   const addTask = useTaskStore((state) => state.handleAddTask)
+  const tasks = useTaskStore((state) => state.tasks)
 
   function handleAddTask() {
+    if (tasks.some((item) => item.description === taskName)) {
+      return Alert.alert(
+        'Tarefa já existe',
+        'Adicione uma tarefa não existente.',
+        [{ text: 'OK' }],
+      )
+    }
     if (taskName.length > 0) {
       addTask(taskName)
       setTaskName('')
